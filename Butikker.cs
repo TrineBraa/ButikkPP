838231f060a34bea677387cb6a82b0e54ea8b80a
trinebra <157688079+trinebra@users.noreply.github.com>
2024-06-20 10:56:01 +0200

    Add project files.

---
+++ Butikker.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ButikkPP
+{
+    
+    internal class Shops
+    {
+        public string _name;
+        public string _type;
+        public int _pricelevel;
+        //List<int> price = new List<int>()
+        //{
+        //    1,2,3
+        //};
+
+        public Shops(string Name, string Type, int PriceLevel)
+        {
+            _name = Name;
+            _type = Type;
+            _pricelevel = PriceLevel;
+        }
+
+        public Shops()
+        {
+            
+        }
+
+         public void ShopMenu(List<Shops>allShops)
+        {
+            PrintAllShops(allShops);
+
+
+        }
+
+        public void PrintAllShops(List<Shops> allShops)
+        {
+            Console.Clear();
+            foreach (var shops in allShops)
+            {
+                Console.WriteLine($"\nShop name: {shops._name}");
+                Console.WriteLine($"Shop Type: {shops._type}");
+                Console.WriteLine($"Price level: {shops._pricelevel}");
+            }
+
+            Console.ReadKey();
+        }
+
+        public void SortingMethod(List<Shops>allShops)
+        {
+            while (true)
+            {
+                Console.WriteLine("choose how to sort the shops");
+                Console.WriteLine("\n Cheap");
+                Console.WriteLine("\n Expensive");
+                var Sorting = Console.ReadLine().ToLower();
+
+                if (Sorting == "cheap")
+                {
+                    Cheap(allShops);
+                }
+                else if (Sorting == "expensive")
+                {
+                    Expensive(allShops);
+                }
+                else
+                {
+                    Console.WriteLine("Please choose one of the options above!");
+                }
+            }
+
+        }
+
+
+        public void Cheap(List<Shops>allShops)
+        {
+
+        }
+
+
+
+        public void Expensive(List<Shops> allShops)
+        {
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,101 @@
+﻿//Denne appen skal gi en oversikt over hvilke butikker som finnes, hva som er spesialiteten deres (klær, matvarer, leker etc) 
+//og prisnivået varene ligger på (1= billig 2=midt på treet 3=sinnsykt dyrt)
+//Man skal også kunne legge til nye butikker, og få mulighet til å velge å se en liste over alle butikker, 
+//    velge å se kun de billigste, velge å se kun de som er midt på treet prismessig eller velge å se bare de butikkene som er rangert som sinnsykt dyre.
+//BONUS
+//Gjør så man også kan sortere/velge å se butikker som har en viss spesialitet f.eks klær i tillegg til å kunne sortere på pris. 
+//    Kjekt å se hvor man kan kjøpe billig mat og dyre designer klær, hehe;)
+
+
+// Klass butikk!
+//Type butikk i objekter (Liste av butikker)
+//Liste av prisnivåer
+//Kunne se alle butikkene
+//Legge til ny butikk
+//Sortere butikkene
+
+
+using ButikkPP;
+
+Shops shop = new Shops();
+List<Shops> allShops = new List<Shops>()
+{
+    new Shops("Meny", "GrocerieShop", 2),
+    new Shops("Joker", "GrocerieShop", 3),
+    new Shops("Rema 1000", "grocerieShop", 1),
+    new Shops("Elkjøp", "electronic shop", 2),
+    new Shops("Power", "electronic shop", 3),
+    new Shops("Komplett", "Electronic shop", 1),
+};
+
+
+Main();
+
+void Main()
+{
+    while (true)
+    {
+        Console.WriteLine("Welcome to the shops app!");
+        Console.WriteLine("\t Here are some options:");
+
+        MainMenu();
+
+    }
+}
+
+
+void MainMenu()
+{
+    Console.Clear();
+    Console.WriteLine("\n\t1. See all shops");
+    Console.WriteLine("\t2. Check prizes");
+    Console.WriteLine("\t3. Add a new shop");
+    Console.WriteLine("\t4. Close");
+    
+    var MainResponse = Console.ReadKey().KeyChar;
+    switch (MainResponse)
+    {
+        case '1':
+            shop.ShopMenu(allShops);
+            break;
+        case '2':
+            shop.SortingMethod(allShops);
+            break;
+        case '3':
+            break;
+        case '4':
+            Console.Clear();
+            Console.WriteLine("Closing the app!");
+            Thread.Sleep(2000);
+            Environment.Exit(404);
+            break;
+        default:
+            Console.WriteLine("please input one of the options above!");
+            break;
+    }
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
